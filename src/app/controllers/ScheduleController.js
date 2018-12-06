1dda75f67901ce9e8c6aeb372225fea7d4a01d15
Ahavaz <lucas.cesar.8@hotmail.com>
2018-12-06 01:41:47 -0200

    Add controllers

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,30 @@
+const { Appointment, User } = require('../models')
+const { Op } = require('sequelize')
+const moment = require('moment')
+
+class ScheduleController {
+  async index (req, res) {
+    const { id } = req.session.user
+    // const date = moment()
+    const appointments = await Appointment.findAll({
+      include: [{ model: User, as: 'user' }],
+      where: {
+        provider_id: id,
+        date: {
+          [Op.between]: [
+            moment()
+              .startOf('day')
+              .format(),
+            moment()
+              .endOf('day')
+              .format()
+          ]
+        }
+      }
+    })
+
+    return res.render('schedule/index', { appointments })
+  }
+}
+
+module.exports = new ScheduleController()
